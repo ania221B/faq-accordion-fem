1392ae6508b5cf7bc9d912989b082ef2866dedc9
ania221B <100519699+ania221B@users.noreply.github.com>
2024-04-07 20:43:08 +0200

    main layout updated+background image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import FaqCard from './FaqCard'
+import bgDesktop from './assets/images/background-pattern-desktop.svg'
+import bgMobile from './assets/images/background-pattern-mobile.svg'
 
 function App () {
   return (
     <main>
-      <section className='section'>
-        <FaqCard></FaqCard>
+      <section className='main-grid'>
+        <picture>
+          <source srcSet={bgDesktop} media='(min-width: 45em)' />
+          <img
+            src={bgMobile}
+            alt='a corridor in a building'
+            className='bg-img'
+          />
+        </picture>
+        <div className='main-content'>
+          <FaqCard></FaqCard>
+        </div>
       </section>
     </main>
   )
